17acd6963ba412225314c5424fc4e75751a842a9
gsold2 <gsold2@gmail.com>
2020-03-04 18:22:13 +0300

    Изменил начальное значение index c -1 на 0 и добавли проверку при сохранении на выход за разрер массива.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,44 +5,44 @@ import java.util.Arrays;
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
-    int index = -1;
+    int index = 0;
 
     void clear() {
-        Arrays.fill(storage, 0, (index + 1), null);
-        index = -1;
+        Arrays.fill(storage, 0, index, null);
+        index = 0;
     }
 
     void save(Resume r) {
         boolean isResumeExist = false;
-        for (int number = 0; number <= index; number++) {
-            if (storage[number].toString().equals(r.toString())) {
-                isResumeExist = true;
-                number = (index + 1);
+        if (index < 1000) {
+            for (int i = 0; i < index; i++) {
+                if (storage[i].toString().equals(r.toString())) {
+                    isResumeExist = true;
+                    i = index;
+                }
             }
         }
-        if (!isResumeExist) {
-            index++;
+        if (!isResumeExist & index < 1000) {
             storage[index] = r;
+            index++;
         }
     }
 
     Resume get(String uuid) {
-        Resume uuidInStorage = null;
-        for (int number = 0; number <= index; number++) {
-            if (storage[number].uuid.equals(uuid)) {
-                uuidInStorage = storage[number];
-                number = (index + 1);
+        for (int i = 0; i < index; i++) {
+            if (storage[i].toString().equals(uuid)) {
+                return storage[i];
             }
         }
-        return uuidInStorage;
+        return null;
     }
 
     void delete(String uuid) {
-        for (int number = 0; number <= index; number++) {
-            if (storage[number].toString().equals(uuid)) {
-                storage[number] = storage[index];
-                storage[index] = null;
-                number = (index + 1);
+        for (int i = 0; i < index; i++) {
+            if (storage[i].toString().equals(uuid)) {
+                storage[i] = storage[index - 1];
+                storage[index - 1] = null;
+                i = index;
                 index--;
             }
         }
@@ -52,10 +52,10 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return Arrays.copyOf(storage, (index + 1));
+        return Arrays.copyOf(storage, index);
     }
 
     int size() {
-        return (index + 1);
+        return index;
     }
 }
