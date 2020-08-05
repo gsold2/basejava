73c8746aefed57dfa96cf5e13bfff24fc1e507fc
gsold2 <gsold2@gmail.com>
2020-08-05 12:25:06 +0400

    Made methods protected.

--- src/ru/javawebinar/basejava/serialization/DataStreamSerialization.java
+++ src/ru/javawebinar/basejava/serialization/DataStreamSerialization.java
@@ -15,8 +15,8 @@ public class DataStreamSerialization implements SerializationStrategy {
             String uuid = dis.readUTF();
             String FullName = dis.readUTF();
             Resume resume = new Resume(uuid, FullName);
-            readMapWithException(resume, dis, this::readContact);
-            readMapWithException(resume, dis, this::readSection);
+            readMapWithException(dis, () -> readContact(resume, dis));
+            readMapWithException(dis, () -> readSection(resume, dis));
             return resume;
         }
     }
@@ -33,19 +33,19 @@ public class DataStreamSerialization implements SerializationStrategy {
         }
     }
 
-    public void writeContact(DataOutputStream dos, Map.Entry<ContactType, String> entry) throws IOException {
+    protected void writeContact(DataOutputStream dos, Map.Entry<ContactType, String> entry) throws IOException {
         dos.writeUTF(entry.getKey().name());
         dos.writeUTF(entry.getValue());
     }
 
-    public void writeSection(DataOutputStream dos, Map.Entry<SectionType, AbstractSection> entry) throws IOException {
+    protected void writeSection(DataOutputStream dos, Map.Entry<SectionType, AbstractSection> entry) throws IOException {
         SectionType sectionType = entry.getKey();
         dos.writeUTF(sectionType.name());
         AbstractSection section = entry.getValue();
         switch (sectionType) {
             case PERSONAL:
             case OBJECTIVE:
-                dos.writeUTF(((TextSection) section).getContent());
+                writeTextSection(dos, (TextSection) section);
                 break;
             case ACHIEVEMENT:
             case QUALIFICATIONS:
@@ -61,41 +61,45 @@ public class DataStreamSerialization implements SerializationStrategy {
         }
     }
 
-    public void writeOrganizationSection(DataOutputStream dos, Organization organization) throws IOException {
+    protected void writeTextSection(DataOutputStream dos, TextSection textSection) throws IOException {
+        dos.writeUTF(textSection.getContent());
+    }
+
+    protected void writeOrganizationSection(DataOutputStream dos, Organization organization) throws IOException {
         dos.writeUTF(organization.getHomePage().getName());
         dos.writeUTF(writeValue(organization.getHomePage().getUrl()));
         writeWithException(dos, organization.getPositions(), position -> writeOrganizationPosition(dos, position));
     }
 
-    public void writeOrganizationPosition(DataOutputStream dos, Organization.Position position) throws IOException {
+    protected void writeOrganizationPosition(DataOutputStream dos, Organization.Position position) throws IOException {
         dos.writeUTF(position.getStartData().toString());
         dos.writeUTF(position.getEndData().toString());
         dos.writeUTF(position.getSubTitel());
         dos.writeUTF(writeValue(position.getDescription()));
     }
 
-    public String writeValue(String value) {
+    protected String writeValue(String value) {
         return value != null ? value : "";
     }
 
     @FunctionalInterface
-    public interface ConsumerWithExceptions<T> {
+    protected interface ConsumerWithExceptions<T> {
         void accept(T t) throws IOException;
     }
 
-    public static <T> void writeWithException(DataOutputStream dos, Collection<T> source,
-                                              ConsumerWithExceptions<T> function) throws IOException {
+    protected <T> void writeWithException(DataOutputStream dos, Collection<T> source,
+                                          ConsumerWithExceptions<T> function) throws IOException {
         dos.writeInt(source.size());
         for (T t : source) {
             function.accept(t);
         }
     }
 
-    public void readContact(Resume resume, DataInputStream dis) throws IOException {
+    protected void readContact(Resume resume, DataInputStream dis) throws IOException {
         resume.addContact(ContactType.valueOf(dis.readUTF()), dis.readUTF());
     }
 
-    public void readSection(Resume resume, DataInputStream dis) throws IOException {
+    protected void readSection(Resume resume, DataInputStream dis) throws IOException {
         SectionType sectionType = SectionType.valueOf(dis.readUTF());
         switch (sectionType) {
             case PERSONAL:
@@ -104,8 +108,7 @@ public class DataStreamSerialization implements SerializationStrategy {
                 break;
             case ACHIEVEMENT:
             case QUALIFICATIONS:
-                List<String> items = readWithException(dis, dis::readUTF);
-                resume.addSection(sectionType, new ListSection(items));
+                readListSection(resume, dis, sectionType);
                 break;
             case EXPERIENCE:
             case EDUCATION:
@@ -116,24 +119,29 @@ public class DataStreamSerialization implements SerializationStrategy {
         }
     }
 
-    public void readTextSection(Resume resume, DataInputStream dis, SectionType sectionType) throws IOException {
+    protected void readTextSection(Resume resume, DataInputStream dis, SectionType sectionType) throws IOException {
         resume.addSection(sectionType, new TextSection(dis.readUTF()));
     }
 
-    public void readOrganizationSection(Resume resume, DataInputStream dis, SectionType sectionType)
+    protected void readListSection(Resume resume, DataInputStream dis, SectionType sectionType) throws IOException {
+        List<String> items = readWithException(dis, dis::readUTF);
+        resume.addSection(sectionType, new ListSection(items));
+    }
+
+    protected void readOrganizationSection(Resume resume, DataInputStream dis, SectionType sectionType)
             throws IOException {
         List<Organization> organizations = readWithException(dis, () -> readOrganization(dis));
         resume.addSection(sectionType, new OrganizationSection(organizations));
     }
 
-    public Organization readOrganization(DataInputStream dis) throws IOException {
+    protected Organization readOrganization(DataInputStream dis) throws IOException {
         String name = dis.readUTF();
         String url = readValue(dis.readUTF());
         List<Organization.Position> organizationPositions = readWithException(dis, () -> readOrganizationPosition(dis));
         return (new Organization(name, url, organizationPositions));
     }
 
-    public Organization.Position readOrganizationPosition(DataInputStream dis) throws IOException {
+    protected Organization.Position readOrganizationPosition(DataInputStream dis) throws IOException {
         YearMonth startData = YearMonth.parse(dis.readUTF());
         YearMonth endData = YearMonth.parse(dis.readUTF());
         String subTitel = dis.readUTF();
@@ -141,17 +149,17 @@ public class DataStreamSerialization implements SerializationStrategy {
         return (new Organization.Position(startData, endData, subTitel, description));
     }
 
-    public String readValue(String value) {
+    protected String readValue(String value) {
         return value.equals("") ? null : value;
     }
 
     @FunctionalInterface
-    public interface SupplierWithExceptions<T> {
+    protected interface SupplierWithExceptions<T> {
         T accept() throws IOException;
     }
 
-    public <T> List<T> readWithException(DataInputStream dis,
-                                         SupplierWithExceptions<T> function) throws IOException {
+    protected <T> List<T> readWithException(DataInputStream dis,
+                                            SupplierWithExceptions<T> function) throws IOException {
         int count = dis.readInt();
         List<T> result = new ArrayList<>();
         for (int i = 0; i < count; i++) {
@@ -161,16 +169,16 @@ public class DataStreamSerialization implements SerializationStrategy {
     }
 
     @FunctionalInterface
-    public interface FunctionReadWithExceptions<R, D> {
-        void accept(R r, D d) throws IOException;
+    protected interface FunctionReadWithExceptions {
+        void accept() throws IOException;
     }
 
-    public void readMapWithException(Resume resume, DataInputStream dis,
-                                     FunctionReadWithExceptions<Resume, DataInputStream> function)
+    protected void readMapWithException(DataInputStream dis,
+                                        FunctionReadWithExceptions function)
             throws IOException {
         int count = dis.readInt();
         for (int i = 0; i < count; i++) {
-            function.accept(resume, dis);
+            function.accept();
         }
     }
 }
--- test/ru/javawebinar/basejava/storage/ResumeTestData.java
+++ test/ru/javawebinar/basejava/storage/ResumeTestData.java
@@ -76,74 +76,74 @@ public class ResumeTestData {
                                 "архитектурных шаблонов, UML, функционального программирования"),
                         ("Родной русский, английский \"upper intermediate\"")));
 
-//        resume.getSections().put(SectionType.EXPERIENCE, new OrganizationSection(
-//                new Organization("Java Online Projects",
-//                        new Organization.Position(YearMonth.parse("2013-10"), YearMonth.now(), "Автор проекта.",
-//                                "Создание, организация и проведение Java онлайн проектов и стажировок.")),
-//                new Organization("Wrike", new Organization.Position(YearMonth.parse("2014-10"),
-//                        YearMonth.parse("2016-01"), "Старший разработчик (backend)",
-//                        "Проектирование и разработка онлайн платформы управления проектами Wrike " +
-//                                "(Java 8 API, Maven, Spring, MyBatis, Guava, Vaadin, PostgreSQL, Redis). " +
-//                                "Двухфакторная аутентификация, авторизация по OAuth1, OAuth2, JWT SSO.")),
-//                new Organization("RIT Center", new Organization.Position(YearMonth.parse("2012-04"),
-//                        YearMonth.parse("2014-10"), "Java архитектор", "Организация процесса " +
-//                        "разработки системы ERP для разных окружений: релизная политика, версионирование, " +
-//                        "ведение CI (Jenkins), миграция базы (кастомизация Flyway), конфигурирование системы " +
-//                        "(pgBoucer, Nginx), AAA via SSO. Архитектура БД и серверной части системы. Разработка " +
-//                        "интергационных сервисов: CMIS, BPMN2, 1C (WebServices), сервисов общего назначения " +
-//                        "(почта, экспорт в pdf, doc, html). Интеграция Alfresco JLAN для online редактирование " +
-//                        "из браузера документов MS Office. Maven + plugin development, Ant, Apache Commons, " +
-//                        "Spring security, Spring MVC, Tomcat,WSO2, xcmis, OpenCmis, Bonita, Python scripting, " +
-//                        "Unix shell remote scripting via ssh tunnels, PL/Python")),
-//                new Organization("Luxoft (Deutsche Bank)",
-//                        new Organization.Position(YearMonth.parse("2010-12"), YearMonth.parse("2012-04"),
-//                                "Ведущий программист", "Участие в проекте Deutsche Bank CRM " +
-//                                "(WebLogic, Hibernate, Spring, Spring MVC, SmartGWT, GWT, Jasper, Oracle). " +
-//                                "Реализация клиентской и серверной части CRM. Реализация RIA-приложения для " +
-//                                "администрирования, мониторинга и анализа результатов в области алгоритмического " +
-//                                "трейдинга. JPA, Spring, Spring-MVC, GWT, ExtGWT (GXT), Highstock, Commet, HTML5.")),
-//                new Organization("Yota", new Organization.Position(YearMonth.parse("2008-06"),
-//                        YearMonth.parse("2010-12"), "Ведущий специалист", "Дизайн и имплементация " +
-//                        "Java EE фреймворка для отдела \"Платежные Системы\" (GlassFish v2.1, v3, OC4J, EJB3, " +
-//                        "JAX-WS RI 2.1, Servlet 2.4, JSP, JMX, JMS, Maven2). Реализация администрирования, статистики " +
-//                        "и мониторинга фреймворка. Разработка online JMX клиента (Python/ Jython, Django, ExtJS)")),
-//                new Organization("Enkata", new Organization.Position(YearMonth.parse("2007-03"),
-//                        YearMonth.parse("2008-06"), "Разработчик ПО", "Реализация клиентской " +
-//                        "(Eclipse RCP) и серверной (JBoss 4.2, Hibernate 3.0, Tomcat, JMS) частей кластерного J2EE " +
-//                        "приложения (OLAP, Data mining).")),
-//                new Organization("Siemens AG", new Organization.Position(YearMonth.parse("2005-01"),
-//                        YearMonth.parse("2007-02"), "Разработчик ПО", "Разработка информационной " +
-//                        "модели, проектирование интерфейсов, реализация и отладка ПО на мобильной IN платформе " +
-//                        "Siemens @vantage (Java, Unix).")),
-//                new Organization("Alcatel", new Organization.Position(YearMonth.parse("1997-09"),
-//                        YearMonth.parse("2005-01"), "Инженер по аппаратному и программному тестированию",
-//                        "Тестирование, отладка, внедрение ПО цифровой телефонной станции " +
-//                                "Alcatel 1000 S12 (CHILL, ASM)."))));
-//
-//        resume.getSections().put(SectionType.EDUCATION,
-//                new OrganizationSection(
-//                        new Organization("Coursera", new Organization.Position(
-//                                YearMonth.parse("2013-03"), YearMonth.parse("2013-05"),
-//                                "\"Functional Programming Principles in Scala\" by Martin Odersky")),
-//                        new Organization("Luxoft", new Organization.Position(
-//                                YearMonth.parse("2011-03"), YearMonth.parse("2011-04"),
-//                                "Курс \"Объектно-ориентированный анализ ИС. Концептуальное " +
-//                                        "моделирование на UML.\"")),
-//                        new Organization("Siemens AG", new Organization.Position(
-//                                YearMonth.parse("2005-01"), YearMonth.parse("2005-04"),
-//                                "3 месяца обучения мобильным IN сетям (Берлин)")),
-//                        new Organization("Alcatel", new Organization.Position(
-//                                YearMonth.parse("1998-09"), YearMonth.parse("1998-09"),
-//                                "6 месяцев обучения цифровым телефонным сетям (Москва)")),
-//                        new Organization("Санкт-Петербургский национальный исследовательский " +
-//                                "университет информационных технологий, механики и оптики",
-//                                new Organization.Position(YearMonth.parse("1993-09"), YearMonth.parse("1996-07"),
-//                                        "Аспирантура (программист С, С++)"),
-//                                new Organization.Position(YearMonth.parse("1987-09"),
-//                                        YearMonth.parse("1993-07"), "Инженер (программист Fortran, C)")),
-//                        new Organization("Заочная физико-техническая школа при МФТИ", new Organization.Position(
-//                                YearMonth.parse("1984-09"), YearMonth.parse("1987-06"),
-//                                "Закончил с отличием"))));
+        resume.getSections().put(SectionType.EXPERIENCE, new OrganizationSection(
+                new Organization("Java Online Projects",
+                        new Organization.Position(YearMonth.parse("2013-10"), YearMonth.now(), "Автор проекта.",
+                                "Создание, организация и проведение Java онлайн проектов и стажировок.")),
+                new Organization("Wrike", new Organization.Position(YearMonth.parse("2014-10"),
+                        YearMonth.parse("2016-01"), "Старший разработчик (backend)",
+                        "Проектирование и разработка онлайн платформы управления проектами Wrike " +
+                                "(Java 8 API, Maven, Spring, MyBatis, Guava, Vaadin, PostgreSQL, Redis). " +
+                                "Двухфакторная аутентификация, авторизация по OAuth1, OAuth2, JWT SSO.")),
+                new Organization("RIT Center", new Organization.Position(YearMonth.parse("2012-04"),
+                        YearMonth.parse("2014-10"), "Java архитектор", "Организация процесса " +
+                        "разработки системы ERP для разных окружений: релизная политика, версионирование, " +
+                        "ведение CI (Jenkins), миграция базы (кастомизация Flyway), конфигурирование системы " +
+                        "(pgBoucer, Nginx), AAA via SSO. Архитектура БД и серверной части системы. Разработка " +
+                        "интергационных сервисов: CMIS, BPMN2, 1C (WebServices), сервисов общего назначения " +
+                        "(почта, экспорт в pdf, doc, html). Интеграция Alfresco JLAN для online редактирование " +
+                        "из браузера документов MS Office. Maven + plugin development, Ant, Apache Commons, " +
+                        "Spring security, Spring MVC, Tomcat,WSO2, xcmis, OpenCmis, Bonita, Python scripting, " +
+                        "Unix shell remote scripting via ssh tunnels, PL/Python")),
+                new Organization("Luxoft (Deutsche Bank)",
+                        new Organization.Position(YearMonth.parse("2010-12"), YearMonth.parse("2012-04"),
+                                "Ведущий программист", "Участие в проекте Deutsche Bank CRM " +
+                                "(WebLogic, Hibernate, Spring, Spring MVC, SmartGWT, GWT, Jasper, Oracle). " +
+                                "Реализация клиентской и серверной части CRM. Реализация RIA-приложения для " +
+                                "администрирования, мониторинга и анализа результатов в области алгоритмического " +
+                                "трейдинга. JPA, Spring, Spring-MVC, GWT, ExtGWT (GXT), Highstock, Commet, HTML5.")),
+                new Organization("Yota", new Organization.Position(YearMonth.parse("2008-06"),
+                        YearMonth.parse("2010-12"), "Ведущий специалист", "Дизайн и имплементация " +
+                        "Java EE фреймворка для отдела \"Платежные Системы\" (GlassFish v2.1, v3, OC4J, EJB3, " +
+                        "JAX-WS RI 2.1, Servlet 2.4, JSP, JMX, JMS, Maven2). Реализация администрирования, статистики " +
+                        "и мониторинга фреймворка. Разработка online JMX клиента (Python/ Jython, Django, ExtJS)")),
+                new Organization("Enkata", new Organization.Position(YearMonth.parse("2007-03"),
+                        YearMonth.parse("2008-06"), "Разработчик ПО", "Реализация клиентской " +
+                        "(Eclipse RCP) и серверной (JBoss 4.2, Hibernate 3.0, Tomcat, JMS) частей кластерного J2EE " +
+                        "приложения (OLAP, Data mining).")),
+                new Organization("Siemens AG", new Organization.Position(YearMonth.parse("2005-01"),
+                        YearMonth.parse("2007-02"), "Разработчик ПО", "Разработка информационной " +
+                        "модели, проектирование интерфейсов, реализация и отладка ПО на мобильной IN платформе " +
+                        "Siemens @vantage (Java, Unix).")),
+                new Organization("Alcatel", new Organization.Position(YearMonth.parse("1997-09"),
+                        YearMonth.parse("2005-01"), "Инженер по аппаратному и программному тестированию",
+                        "Тестирование, отладка, внедрение ПО цифровой телефонной станции " +
+                                "Alcatel 1000 S12 (CHILL, ASM)."))));
+
+        resume.getSections().put(SectionType.EDUCATION,
+                new OrganizationSection(
+                        new Organization("Coursera", new Organization.Position(
+                                YearMonth.parse("2013-03"), YearMonth.parse("2013-05"),
+                                "\"Functional Programming Principles in Scala\" by Martin Odersky")),
+                        new Organization("Luxoft", new Organization.Position(
+                                YearMonth.parse("2011-03"), YearMonth.parse("2011-04"),
+                                "Курс \"Объектно-ориентированный анализ ИС. Концептуальное " +
+                                        "моделирование на UML.\"")),
+                        new Organization("Siemens AG", new Organization.Position(
+                                YearMonth.parse("2005-01"), YearMonth.parse("2005-04"),
+                                "3 месяца обучения мобильным IN сетям (Берлин)")),
+                        new Organization("Alcatel", new Organization.Position(
+                                YearMonth.parse("1998-09"), YearMonth.parse("1998-09"),
+                                "6 месяцев обучения цифровым телефонным сетям (Москва)")),
+                        new Organization("Санкт-Петербургский национальный исследовательский " +
+                                "университет информационных технологий, механики и оптики",
+                                new Organization.Position(YearMonth.parse("1993-09"), YearMonth.parse("1996-07"),
+                                        "Аспирантура (программист С, С++)"),
+                                new Organization.Position(YearMonth.parse("1987-09"),
+                                        YearMonth.parse("1993-07"), "Инженер (программист Fortran, C)")),
+                        new Organization("Заочная физико-техническая школа при МФТИ", new Organization.Position(
+                                YearMonth.parse("1984-09"), YearMonth.parse("1987-06"),
+                                "Закончил с отличием"))));
 
         return resume;
     }
