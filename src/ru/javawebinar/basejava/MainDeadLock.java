20c014c88b0c7c767b06c6211b069314a53a70b8
gsold2 <gsold2@gmail.com>
2020-08-15 21:18:55 +0400

    Wrote deadlock.

---
+++ src/ru/javawebinar/basejava/MainDeadLock.java
@@ -0,0 +1,33 @@
+package ru.javawebinar.basejava;
+
+public class MainDeadLock {
+
+    public final static Object one = new Object(), two = new Object();
+
+    public static void main(String[] args) {
+
+        Thread t1 = new Thread(() -> {
+            synchronized (one) {
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (two) {
+                    System.out.println("Завершение работы t1");
+                }
+            }
+        });
+        Thread t2 = new Thread(() -> {
+            synchronized (two) {
+                synchronized (one) {
+                    System.out.println("Завершение работы t2");
+                }
+            }
+        });
+
+        t1.start();
+        t2.start();
+    }
+}
+
