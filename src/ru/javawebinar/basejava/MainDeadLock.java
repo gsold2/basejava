3dd7b463a5ce0fc1e172636f52aea2a6478e514e
gsold2 <gsold2@gmail.com>
2020-08-17 21:37:01 +0400

    Rewrote with sleep().

--- src/ru/javawebinar/basejava/MainDeadLock.java
+++ src/ru/javawebinar/basejava/MainDeadLock.java
@@ -3,18 +3,16 @@ package ru.javawebinar.basejava;
 public class MainDeadLock {
 
     public final static Object one = new Object(), two = new Object();
-    public static boolean flageOne = false, flageTwo = false;
 
     public static void main(String[] args) {
 
         Thread t1 = new Thread(() -> {
             synchronized (one) {
-                flageOne = true;
-                System.out.println("Поток t1 зашел в synchronized (one)");
-                while (!flageTwo) {
-                    System.out.println("Поток t1 ожидает пока t2 synchronized (two)");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
-                System.out.println("Поток t1 пытается synchronized (two)");
                 synchronized (two) {
                     System.out.println("Завершение работы t1");
                 }
@@ -22,12 +20,11 @@ public class MainDeadLock {
         });
         Thread t2 = new Thread(() -> {
             synchronized (two) {
-                flageTwo = true;
-                System.out.println("Поток t2 зашел в synchronized (two)");
-                while (!flageOne) {
-                    System.out.println("Поток t2 ожидает пока t1 synchronized (one)");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
-                System.out.println("Поток t2 пытается synchronized (one)");
                 synchronized (one) {
                     System.out.println("Завершение работы t2");
                 }
