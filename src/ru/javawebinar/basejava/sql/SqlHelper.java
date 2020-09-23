41a6257ec60648b9febb062568c2ee104467a177
gsold2 <gsold2@gmail.com>
2020-09-23 23:21:25 +0400

    SqlHelper.java was cleaned

--- src/ru/javawebinar/basejava/sql/SqlHelper.java
+++ src/ru/javawebinar/basejava/sql/SqlHelper.java
@@ -1,9 +1,7 @@
 package ru.javawebinar.basejava.sql;
 
-import org.postgresql.util.PSQLException;
 import ru.javawebinar.basejava.exception.ExistStorageException;
 import ru.javawebinar.basejava.exception.StorageException;
-import ru.javawebinar.basejava.sql.ConnectionFactory;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
